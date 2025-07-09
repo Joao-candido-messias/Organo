58e78b83b1f4001cd063ef14595965ebdc399ea8
João Messias <joao.2020317758@aluno.iffar.edu.br>
2025-07-09 12:32:28 -0300

    Aula 4

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,10 +1,11 @@
+import Time from '../Time';
 import './colaborador.css'
 import { IoCloseCircle } from "react-icons/io5";
 
 const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
 
     return (<div className="colaborador">
-        <IoCloseCircle size={25} className='deletar' onClick={aoDeletar}/>
+        <IoCloseCircle size={25} className='deletar' onClick={() => aoDeletar(colaborador.id)}/>
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
